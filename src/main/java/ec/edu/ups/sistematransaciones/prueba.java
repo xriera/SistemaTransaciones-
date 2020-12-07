4ef05cbf4c4b217611613eb42a4eeeb08603ed00
xriera <xavier94xr8@gmail.com>
2020-12-06 23:30:24 -0500

    MERGE VINI 2

--- src/main/java/ec/edu/ups/sistematransaciones/prueba.java
+++ src/main/java/ec/edu/ups/sistematransaciones/prueba.java
@@ -12,7 +12,7 @@ package ec.edu.ups.sistematransaciones;
 public class prueba {
     public static void main(String[] args) {
         System.out.println("ec.edu.ups.sistematransaciones.prueba.main()");
-        //CALLA CAMAS 
+        //CALLA CAMAS !!!!!!!!!!!!!!
         System.out.println("Yo le puteo by Wilmer Camas");
     }
 }
